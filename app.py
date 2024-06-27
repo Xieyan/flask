cb7fbea46a7902844842743d2ec8d6d483225aab
Xie Yan <xieyan@microsoft.com>
2024-06-27 21:12:28 +0800

    init

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, url_for, request, render_template, session
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def index_post():
+    # Read the values from the form
+    original_text = request.form['text']
+    target_language = request.form['language']
+
+    translated_text = "你好世界"
+    return render_template(
+        'results.html',
+        translated_text=translated_text,
+        original_text=original_text,
+        target_language=target_language
+    )
